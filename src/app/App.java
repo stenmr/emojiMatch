127d3437d1885ff2b55465a8221e7188c503eacd
Vechro <stenmr@gmail.com>
2020-04-06 23:08:41 +0300

    Another reorganization

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
-
         Game game = new Game();
         game.initializeDeck(new String[] { "S", "P", "K", "Q", "U" }, 10);
         game.start();
---
+++ src/test/GameTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    @Test
+    public void test() {
+        assertEquals(true, false);
+    }
+}
