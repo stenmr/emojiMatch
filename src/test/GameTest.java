2fef499c9e9c497e5a2a64af5fd95be329321a85
Vechro <stenmr@gmail.com>
2020-04-06 23:23:28 +0300

    A few tests

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void test() {
+    public void deckSize() {
 
         Deck deck = new Deck(new String[] {"S", "T"}, 4);
 
-        assertEquals("Deck size is 4: ", deck.size(), 4);
+        assertEquals("Deck size is correct", deck.size(), 4);
+    }
+
+    @Test
+    public void deckIndexing() {
+        Deck deck = new Deck(new String[] {"Q", "Y"}, 4);
+
+        assertEquals("Indexing is correct", deck.get(1), "Y");
     }
 }
